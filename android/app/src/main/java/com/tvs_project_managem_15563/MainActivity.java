565361707cb884a31dc83de051f2fb0414d5270c
Crowdbotics <team@crowdbotics.com>
2020-04-07 20:21:14 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/tvs_project_managem_15563/MainActivity.java
@@ -0,0 +1,15 @@
+package com.tvs_project_managem_15563;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "tvs_project_managem_15563";
+    }
+}
